6780bb8e9881890d41fdbca0c2b729b77a03f87f
root <root@pplxconfig3.physics.ox.ac.uk>
2013-11-07 15:29:19 +0000

    added policy file

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,4 +13,16 @@
 class argus inherits argus::params {
   include argus::install
 
+## Temporary mechanism untill proper module for policy management
+
+file { '/root/argus_policy_file':
+      ensure  => 'present',
+      source  => 'puppet:///site_files/grid/argus_policy_file',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      
+  }
+
+
 }
